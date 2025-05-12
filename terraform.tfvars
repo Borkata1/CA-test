106dc7deec7a3fc06ddb97321a2f008d42f894a0
Your Name <you@example.com>
2025-05-12 13:31:59 +0000

    terraform.tfvar

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+gcp_project_id       = "wideops-support-3934126"
+gcp_region           = "europe-central2"
+vpc_name             = "CustomVPC"
+subnet_name          = "CustomSubnet"
+subnet_ip_cidr_range = "10.0.0.0/24"
+lb_ip                = "LB-IP"
--- variables.tf
+++ variables.tf
@@ -1,36 +1,30 @@
 variable "gcp_project_id" {
-  type = string
+  type        = string
   description = "The ID of your Google Cloud project."
-  default     = "wideops-support-393412"
 }
 
 variable "gcp_region" {
   type        = string
   description = "The default GCP region to use for resources."
-  default     = "europe-west1" # Choose a region suitable for you
 }
 
 variable "vpc_name" {
   type        = string
   description = "The name to use for the custom VPC network."
-  default     = "my-custom-vpc"
 }
 
 variable "subnet_name" {
   type        = string
   description = "The name to use for the custom subnetwork."
-  default     = "my-custom-subnet-1"
 }
 
 variable "subnet_ip_cidr_range" {
   type        = string
   description = "The IP CIDR range for the custom subnetwork."
-  default     = "10.0.0.0/24"
 }
 
 variable "lb_ip" {
   type        = string
-  description = "The the reserved ip for the load balancer"
-  default     = "external-lb-ip"
+  description = "The reserved IP for the load balancer."
 }
 # Optional: You can add more variables for other configurations later
